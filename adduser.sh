2ed54bac937c0465b517ad9496b67d382cfaa122
najamuddin sapia'i <binpda.shark@gmail.com>
2015-04-10 15:21:52 +0800

    Update adduser.sh
    major changes

--- adduser.sh
+++ adduser.sh
@@ -18,16 +18,30 @@ do
 clear
 	echo "Please provide all the information that is requested"
 	echo ""
-	echo "Full Name:"
-	read fullname
-	echo "Username without space:"
-	read username
+	if [ $(id -u) -eq 0 ]; then
+		read -p "Enter username : " username
+		read -s -p "Enter password : " password
+		egrep "^$username" /etc/passwd >/dev/null
+		if [ $? -eq 0 ]; then
+			echo "$username exists!"
+			exit 1
+		else
+			pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+			useradd -m -p $pass $username
+			[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+		fi
+	else
+		echo "Only root may add a user to the system"
+		exit 2
+	fi
+	
 	echo "Group:"
 	echo "1)HR"
 	echo "2)Engineering"
 	echo "3)IT"
 	echo "4)Finance"
 	read -p "Select an option [1-4]:" option
+	
 	if [$option = 1]; then $group="HR"
 	elif [$option = 2]; then $group="Engineering"
 	elif [$option = 3]; then $group="IT"
@@ -37,6 +51,5 @@ clear
 	fi
 done
 
-adduser --home /home/$username/ --ingroup $group --gecos $username
-passwd $username
+useradd -G $group $username
 
